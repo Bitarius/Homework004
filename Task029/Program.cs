886cc049f6bac4ad3d47f74b4894a2f90a0e742d
Bitarius <bitarius@gmail.com>
2022-12-15 23:46:06 +0300

    homework004 completed

---
+++ Task029/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    array[i] = int.Parse(Console.ReadLine() ??"");;
+}
+
+
+
+//метод печати массива
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+
+PrintArray(array);
